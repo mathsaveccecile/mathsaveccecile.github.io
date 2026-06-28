cd7b243d0f88a589dc63f0265e44134f6eb5c3ad
mathsaveccecile <tetiloug@gmail.com>
2026-06-28 21:35:55 +0200

    ✨

--- pythagore-data.js
+++ pythagore-data.js
@@ -2,32 +2,36 @@ const capsuleData = {
   title: "Pythagore",
   levels: "4e / 3e",
   duration: "18 min",
+
   steps: [
+    {
+      type: "image",
+      title: "Comment fonctionne la capsule ?",
+      image: "pythagore.png"
+    },
+
     {
       type: "video",
       title: "0. Introduction",
       youtube: ""
     },
-    {
-      type: "fiche",
-      title: "Fiche 1",
-      file: ""
-    },
-    {
-      type: "image",
-      title: "Observer avant de calculer",
-      image: ""
-    },
+
     {
       type: "quiz",
       title: "Quiz 1",
-      question: "Peut-on utiliser Pythagore dans n'importe quel triangle ?",
+      question: "Peut-on utiliser le théorème de Pythagore dans n'importe quel triangle ?",
       options: [
         "Oui, toujours",
         "Non, seulement dans un triangle rectangle",
         "Seulement dans un triangle isocèle"
       ],
-      answer: 1
+      answer: "Non, seulement dans un triangle rectangle"
+    },
+
+    {
+      type: "fiche",
+      title: "Fiche 1",
+      file: ""
     }
   ]
 };
