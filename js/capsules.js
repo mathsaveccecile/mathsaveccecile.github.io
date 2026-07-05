543313064bfda0f9c0e5965141b3527125e4e940
mathsaveccecile <tetiloug@gmail.com>
2026-07-05 19:14:03 +0200

    bug 3

--- js/capsules.js
+++ js/capsules.js
@@ -12,7 +12,7 @@ async function chargerCapsules(niveau) {
         .forEach(capsule => {
 
             liste.innerHTML += `
-                <div class="card" style="background:none;box-shadow:none;padding:0;">
+                <div class="card" style="background:none;box-shadow:none;padding:0;max-width:340px;margin:0 auto;">
 
                     <a href="${capsule.page}">
 
@@ -20,13 +20,13 @@ async function chargerCapsules(niveau) {
                             src="${capsule.thumbnail}"
                             alt="${capsule.title}"
                             style="
-  width:100%;
-  max-width:340px;
+  style="
+  width:340px;
+  height:340px;
+  object-fit:cover;
   border-radius:25px;
-  transition:.3s;
   box-shadow:0 18px 35px rgba(0,0,0,.20);
   display:block;
-  margin:0 auto;
                             ">
 
                     </a>
