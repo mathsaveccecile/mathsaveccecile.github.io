8e0ad8575959a8fe115a91b38e99bbc4da59199c
mathsaveccecile <tetiloug@gmail.com>
2026-07-05 21:35:13 +0200

    bug6

--- js/capsules.js
+++ js/capsules.js
@@ -15,21 +15,20 @@ async function chargerCapsules(niveau) {
             liste.innerHTML += `
                 <div class="card" style="background:none;box-shadow:none;padding:0;max-width:340px;margin:0 auto;">
 
-                    <a href="Pythagore.html?data=${capsule.dataFile}">
+                    <a href="capsule.html?data=${capsule.dataFile}">
 
                         <img
-                            src="${capsule.thumbnail}"
-                            alt="${capsule.title}"
-                            style="
-  style="
-  width:260px;
-  height:260px;
-  object-fit:cover;
-  border-radius:25px;
-  box-shadow:0 18px 35px rgba(0,0,0,.20);
-  display:block;
-">
-
+    src="${capsule.thumbnail}"
+    alt="${capsule.title}"
+    style="
+        width:260px;
+        height:260px;
+        object-fit:cover;
+        border-radius:25px;
+        box-shadow:0 18px 35px rgba(0,0,0,.20);
+        display:block;
+        margin:auto;
+    ">
                     </a>
 
                 </div>
