3899317db53ac55c1a727b21ac98159add847552
mathsaveccecile <tetiloug@gmail.com>
2026-07-06 12:17:57 +0200

    essai ultime!

--- js/capsules.js
+++ js/capsules.js
@@ -1,39 +1,32 @@
 console.log("CAPSULES.JS CHARGÉ");
+
 async function chargerCapsules(niveau) {
+  const reponse = await fetch("capsules.json");
+  const capsules = await reponse.json();
 
-    const reponse = await fetch("capsules.json");
-    const capsules = await reponse.json();
+  const liste = document.getElementById("listeCapsules");
+  liste.innerHTML = "";
 
-    const liste = document.getElementById("listeCapsules");
-
-    liste.innerHTML = "";
-
-    capsules
-        .filter(c => c.levels.includes(niveau))
-        .forEach(capsule => {
-
-            liste.innerHTML += `
-                <div class="card" style="background:none;box-shadow:none;padding:0;max-width:340px;margin:0 auto;">
-
-                    <a href="capsule.html?data=${capsule.dataFile}">
-
-                        <img
-    src="${capsule.thumbnail}"
-    alt="${capsule.title}"
-    style="
-        width:260px;
-        height:260px;
-        object-fit:cover;
-        border-radius:25px;
-        box-shadow:0 18px 35px rgba(0,0,0,.20);
-        display:block;
-        margin:auto;
-    ">
-                    </a>
-
-                </div>
-            `;
-
-        });
-
+  capsules
+    .filter(c => c.levels.includes(niveau))
+    .forEach(capsule => {
+      liste.innerHTML += `
+        <div class="card" style="background:none;box-shadow:none;padding:0;max-width:340px;margin:0 auto;">
+          <a href="capsule.html?data=${capsule.dataFile}">
+            <img
+              src="${capsule.thumbnail}"
+              alt="${capsule.title}"
+              style="
+                width:260px;
+                height:260px;
+                object-fit:cover;
+                border-radius:25px;
+                box-shadow:0 18px 35px rgba(0,0,0,.20);
+                display:block;
+                margin:auto;
+              ">
+          </a>
+        </div>
+      `;
+    });
 }
