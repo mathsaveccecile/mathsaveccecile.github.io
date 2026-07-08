e2c3856b9d3b734442f6c629072042d9b4991d71
mathsaveccecile <tetiloug@gmail.com>
2026-07-08 18:03:10 +0200

    barre

--- js/capsules.js
+++ js/capsules.js
@@ -13,29 +13,6 @@ async function chargerCapsules(niveau) {
       liste.innerHTML += `
 <div class="card" style="background:none;box-shadow:none;padding:0;max-width:340px;margin:0 auto;">
 
-<a href="capsule.html?data=${capsule.dataFile}">
-<img
-src="${capsule.thumbnail}"
-alt="${capsule.title}"
-style="
-width:260px;
-height:260px;
-object-fit:cover;
-border-radius:25px;
-box-shadow:0 18px 35px rgba(0,0,0,.20);
-display:block;
-margin:auto;
-">
-</a>
-
-<h3 style="
-margin-top:18px;
-font-size:22px;
-color:#2738c9;
-">
-${capsule.title}
-</h3>
-
 <div style="
 margin-top:18px;
 height:18px;
@@ -61,6 +38,23 @@ color:#2738c9;
 ">
 0 % terminé
 </div>
+
+<a href="capsule.html?data=${capsule.dataFile}">
+<img
+src="${capsule.thumbnail}"
+alt="${capsule.title}"
+style="
+width:260px;
+height:260px;
+object-fit:cover;
+border-radius:25px;
+box-shadow:0 18px 35px rgba(0,0,0,.20);
+display:block;
+margin:auto;
+">
+</a>
+
+
 
 </div>
 `;
