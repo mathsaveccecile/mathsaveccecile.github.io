d108d45572a46e63df204affd1081ed0f5f77a8c
mathsaveccecile <tetiloug@gmail.com>
2026-07-08 18:10:21 +0200

    top

--- js/capsules.js
+++ js/capsules.js
@@ -11,52 +11,39 @@ async function chargerCapsules(niveau) {
     .filter(c => c.levels.includes(niveau))
     .forEach(capsule => {
       liste.innerHTML += `
-<div class="card" style="background:none;box-shadow:none;padding:0;max-width:340px;margin:0 auto;">
+        <div class="card" style="background:none;box-shadow:none;padding:0;max-width:340px;margin:0 auto;">
 
-<div style="
-margin-top:18px;
-height:18px;
-background:#e6e6e6;
-border-radius:999px;
-overflow:hidden;
-">
+          <div style="
+            height:18px;
+            background:#e6e6e6;
+            border-radius:999px;
+            overflow:hidden;
+            margin-bottom:18px;
+          ">
+            <div style="
+              width:0%;
+              height:100%;
+              background:linear-gradient(90deg,#ff4fa3,#3058ff);
+              border-radius:999px;
+            "></div>
+          </div>
 
-<div style="
-width:0%;
-height:100%;
-background:linear-gradient(90deg,#ff4fa3,#3058ff);
-border-radius:999px;
-"></div>
+          <a href="capsule.html?data=${capsule.dataFile}">
+            <img
+              src="${capsule.thumbnail}"
+              alt="${capsule.title}"
+              style="
+                width:260px;
+                height:260px;
+                object-fit:cover;
+                border-radius:25px;
+                box-shadow:0 18px 35px rgba(0,0,0,.20);
+                display:block;
+                margin:auto;
+              ">
+          </a>
 
-</div>
-
-<div style="
-margin-top:10px;
-font-size:18px;
-font-weight:900;
-color:#2738c9;
-">
-0 % terminé
-</div>
-
-<a href="capsule.html?data=${capsule.dataFile}">
-<img
-src="${capsule.thumbnail}"
-alt="${capsule.title}"
-style="
-width:260px;
-height:260px;
-object-fit:cover;
-border-radius:25px;
-box-shadow:0 18px 35px rgba(0,0,0,.20);
-display:block;
-margin:auto;
-">
-</a>
-
-
-
-</div>
-`;
+        </div>
+      `;
     });
 }
